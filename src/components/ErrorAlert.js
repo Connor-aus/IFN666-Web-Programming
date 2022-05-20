50f4e3eda7806d480d13fd73b0b16c4067dd53c6
Connor-aus <cmm.mcsweeney@gmail.com>
2022-05-20 21:32:16 +1000

    Fixed error size

--- src/components/ErrorAlert.js
+++ src/components/ErrorAlert.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
 import Alert from "react-bootstrap/Alert";
+import { useState } from "react";
+import { Container, Col, Row } from "react-bootstrap";
 
 export default function ErrorAlert(error) {
   const [show, setShow] = useState(true);
 
   if (show) {
     return (
-      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-        <Alert.Heading>Something went wrong</Alert.Heading>
-        <br />
-        <p>Error: {error.data}</p>
-        <p>Please try refresh or navigate to another page.</p>
-      </Alert>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col sm="auto" id="skinny">
+            <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+              <Alert.Heading>Something went wrong</Alert.Heading>
+              <br />
+              <p>Error: {error.data}</p>
+              <p>Please try refresh or navigate to another page.</p>
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
     );
   }
   return;
